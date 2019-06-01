650f344d2cde41c78095728e7ebf4db0981b8496
xx <xx@macdeMacBook-Pro-2.local>
2019-06-01 18:35:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+#教程地址[https://zhuanlan.zhihu.com/p/67747788](https://zhuanlan.zhihu.com/p/67747788)
---
+++ myscript.js
@@ -0,0 +1,5 @@
+
+window.setInterval(()=>{
+  var btn=document.querySelector(".ytp-ad-skip-button");if(btn){btn.click()}
+},1000);
+ 
