fd315476a7ff1ba8270874a096233e9c2ad3e05d
xx <xx@macdeMacBook-Pro-2.local>
2019-06-22 14:57:26 +0800

    add

--- myscript.js
+++ myscript.js
@@ -3,3 +3,7 @@ window.setInterval(()=>{
   var btn=document.querySelector(".ytp-ad-skip-button");if(btn){btn.click()}
 },1000);
  
+var list=document.querySelectorAll("a");
+list.forEach(e => {
+   e.target="_blank";
+});
